b9c60659e9339670152ce5f2ded48533551ef23c
LuisHenriique <luishenrique162004@gmail.com>
2025-06-05 00:22:12 -0300

    Implementação do web Scraping - Coleta de dados da aba grade curricular

--- usp.py
+++ usp.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
 
+from selenium.common import TimeoutException
+
 """Minhas classes """
 from unit import Unit
 from subject import Subject
-from course import Course
+from course import Coursedriver.implicitly_wait(25)  # seconds
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time  # usado para esperar entre ações
+import time
 
 i = 0
 j = 0
@@ -18,20 +19,18 @@ j = 0
 # vai pra o navegador
 driver = webdriver.Chrome()
 
-#vai para o link abaixo
+# vai para o link abaixo
 driver.get("https://uspdigital.usp.br/jupiterweb/jupCarreira.jsp?codmnu=8275")
 
 # aguarda o site carregar completamente
-driver.implicitly_wait(20) # seconds
+time.sleep(5)
 
-
-selectUnits =Select(driver.find_element(By.ID, "comboUnidade"))
-
+selectUnits = Select(driver.find_element(By.ID, "comboUnidade"))
 
 # Itera sobre TODAS as unidades (exceto a primeira que é vazia )
 
 
-for units in selectUnits.options[1:]: # pula primeiro item da lista de options(lista de unidades)
+for units in selectUnits.options[1:]:  # pula primeiro item da lista de options(lista de unidades)
 
     unitName = units.text
     unitValue = units.get_attribute("value")
@@ -40,7 +39,7 @@ for units in selectUnits.options[1:]: # pula primeiro item da lista de options(l
 
     # Seleciona a nova unidade
     selectUnits.select_by_value(unitValue)
-    i+=1
+    i += 1
     # Espera até o número de cursos mudar — isso garante que os cursos novos carregaram
     WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.ID, "comboCurso")))
     time.sleep(0.4)
@@ -59,27 +58,30 @@ for units in selectUnits.options[1:]: # pula primeiro item da lista de options(l
         # Após ter selecionada o curso clica no butão buscar
         searchButtonOne = driver.find_element(By.ID, "enviar")
         searchButtonOne.click()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "step1-tab")))
-        time.sleep(0.5)
 
-        # encontra o link de grades curriculares e clica nele para acessar a página a qual seá lida as informações
-        gradesCurriculum = driver.find_element(By.ID, "step4-tab")
+        WebDriverWait(driver, 7).until(EC.presence_of_element_located((By.ID, "step4-tab")))
 
-        # Teste para verificar possível erro: espera que overlay desapareça
+        try:  ## tentar achar o botão de grades
 
-        time.sleep(0.7)
-        gradesCurriculum.click()
-        time.sleep(0.6)
+            gradesCurriculum = WebDriverWait(driver, 7).until(
+                EC.element_to_be_clickable((By.ID, "step4-tab"))
+            )
+
+            gradesCurriculum.click()
+            time.sleep(1)
+        except:
+            # se não achou procura o botão de voltar
+            closeButton = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[.//text()='Fechar']"))
+            )
+            closeButton.click()
+            time.sleep(0.5)
 
         # usa o beautiful para ler baixar a página web
 
         # retorna para aba buscar
         searchButtonTwo = driver.find_element(By.ID, "step1-tab")
         searchButtonTwo.click()
-
-
-
-driver.quit()
 
 print(f"numeros de unidades:{i}")
 print(f"numeros de cursos{j}")
